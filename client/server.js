cf5ac26f54d6af946cf99cdbbced808d0fbb386e
gameisboring <daum128@naver.com>
2022-03-16 15:46:04 +0900

    client-server 수정 & rtmp-server 수정

--- client/server.js
+++ client/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use('/data', express.static('data'))
+app.use('../data', express.static('data'))
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
